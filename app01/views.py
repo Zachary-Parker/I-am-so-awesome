dbdfe78093414b2644b199332a91a4ad8381c43b
sdfsdf <parker@123.com>
2018-04-15 17:09:40 +0800

    改完了

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    if request.method == 'GET':
+        return render(request,'index.html')
+    user = request.POST.get('username')
+    pwd = request.POST.get('password')
+    if user == 'Parker' and pwd == '123':
+        return HttpResponse('Welcome!')
+    return HttpResponse('Login Failed')
